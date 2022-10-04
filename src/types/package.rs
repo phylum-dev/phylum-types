1bc96d8158bba84c3382ae26f9f3d39d7a17137e
Matt Donoughe <matt.donoughe@phylum.io>
2022-10-04 08:28:26 -0400

    add missing fields from live API (#51)
    * add missing fields from released API
    
    * implement Eq too

--- src/types/package.rs
+++ src/types/package.rs
@@ -1,8 +1,9 @@
 //! Module containing data types reprsenting on-the-wire data for packages
 
+use std::collections::HashMap;
+use std::convert::TryFrom;
 use std::fmt;
 use std::str::FromStr;
-use std::{collections::HashMap, convert::TryFrom};
 
 use chrono::{DateTime, Utc};
 use schemars::JsonSchema;
@@ -359,6 +360,19 @@ pub struct Package {
     pub developer_responsiveness: Option<DeveloperResponsiveness>,
     pub issue_impacts: IssueImpacts,
     pub complete: bool,
+    pub release_data: Option<PackageReleaseData>,
+    pub repo_url: Option<String>,
+    #[serde(rename = "maintainers_recently_changed")]
+    pub maintainers_recently_changed: Option<bool>,
+    #[serde(rename = "is_abandonware")]
+    pub is_abandonware: Option<bool>,
+}
+
+#[derive(PartialEq, PartialOrd, Clone, Debug, Default, Eq, Serialize, Deserialize, JsonSchema)]
+#[serde(default)]
+pub struct PackageReleaseData {
+    pub first_release_date: String,
+    pub last_release_date: String,
 }
 
 // v--- TODO: OLD PACKAGE RESPONSES ---v //
