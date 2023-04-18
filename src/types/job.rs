984644ce91246256547fc323421ed360469498a9
Matthew Donoughe <matt.donoughe@phylum.io>
2023-04-18 10:37:01 -0400

    put back use that was removed for other pr

--- src/types/job.rs
+++ src/types/job.rs
@@ -5,7 +5,7 @@ use serde::{Deserialize, Serialize};
 
 use super::common::*;
 use super::project::*;
-use crate::types::package::{PackageDescriptor, PackageStatus, PackageStatusExtended};
+use crate::types::package::{PackageDescriptor, PackageStatus, PackageStatusExtended, PackageType};
 
 /// When a job is completed, and some requirement is not met ( such as quality
 /// level ), what action should be taken?
